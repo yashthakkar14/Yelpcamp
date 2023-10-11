6ae9e1fa8c17e6c31226212f48b83c6fc3af5b27
Yash Thakkar <yasht1407@gmail.com>
2023-10-12 00:36:01 +0530

    S39 - Campground New and Create

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const app = express();
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended:true}))
+
 app.get('/', (req, res)=>{
     res.render('home')
 })
@@ -25,6 +27,17 @@ app.get('/', (req, res)=>{
 app.get('/campgrounds', async (req, res)=>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
+})
+
+app.post('/campgrounds', async(req, res)=>{
+    // We can access it like below as we are sending the data by grouping with campground
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/new', (req, res)=>{
+    res.render('campgrounds/new');
 })
 
 app.get('/campgrounds/:id', async(req, res)=>{
