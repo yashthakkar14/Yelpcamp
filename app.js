eb8cb1ef877d3f168c4d9c3e0c4554f4f7135197
Yash Thakkar <yasht1407@gmail.com>
2023-10-12 00:00:55 +0530

    S29 - Campground Show

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ app.get('/campgrounds', async (req, res)=>{
     res.render('campgrounds/index', {campgrounds});
 })
 
+app.get('/campgrounds/:id', async(req, res)=>{
+    const id = req.params.id;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', {campground});
+})
 
 app.listen(3000, ()=>{
     console.log('Serving on port 3000')
