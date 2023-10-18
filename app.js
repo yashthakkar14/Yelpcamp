7be94ed53b2209705b6c9bc15870f6c25ea5a024
Yash Thakkar <yasht1407@gmail.com>
2023-10-18 17:10:07 +0530

    JOI Schema Validations

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
@@ -36,7 +37,25 @@ app.get('/campgrounds', catchAsync(async (req, res)=>{
 }));
 
 app.post('/campgrounds', catchAsync(async(req, res, next)=>{
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // Basic rudimentary logic
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+
+    const campgroundSchema = Joi.object({
+        // We are having the campground key in body where we are grouing everything with campground in the html new campground form.
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string.required(),
+            location: Joi.string.required(),
+            description: Joi.string.required()
+        }).required()
+    })
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        // We need to do the below as details is basically an array of objects.
+        const msg = error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
