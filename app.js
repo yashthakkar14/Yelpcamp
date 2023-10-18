1a4a40ffaf993d1d05f0486612fbaef2836c99ee
Yash Thakkar <yasht1407@gmail.com>
2023-10-18 14:59:39 +0530

    S43 - Defining ExpressError Class

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate')
+const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override')
 const Campground = require('./models/campground');
 
@@ -28,50 +29,45 @@ app.get('/', (req, res)=>{
     res.render('home')
 })
 
-app.get('/campgrounds', async (req, res)=>{
+app.get('/campgrounds', catchAsync(async (req, res)=>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
-})
+}));
 
-app.post('/campgrounds', async(req, res, next)=>{
-    try {
-        const campground = new Campground(req.body.campground);
-        await campground.save();
-        res.redirect(`/campgrounds/${campground._id}`)
-    }
-    catch(e){
-        next(e);
-    }
-})
+app.post('/campgrounds', catchAsync(async(req, res, next)=>{
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+}))
 
 app.get('/campgrounds/new', (req, res)=>{
     res.render('campgrounds/new');
 })
 
-app.get('/campgrounds/:id/edit', async (req, res)=>{
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res)=>{
     const id = req.params.id;
     const campground = await Campground.findById(id);
     res.render('campgrounds/edit', {campground});
-})
+}));
 
 
-app.get('/campgrounds/:id', async(req, res)=>{
+app.get('/campgrounds/:id', catchAsync(async(req, res)=>{
     const id = req.params.id;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show', {campground});
-})
+}));
 
-app.put('/campgrounds/:id', async(req, res)=>{
+app.put('/campgrounds/:id', catchAsync(async(req, res)=>{
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new:true});
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}));
 
-app.delete('/campgrounds/:id', async(req, res)=>{
+app.delete('/campgrounds/:id', catchAsync(async(req, res)=>{
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds')
-})
+}));
 
 // Error handling middleware
 app.use((err, req, res, next)=>{
