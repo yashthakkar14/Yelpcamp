ad6f18ad7e749d5cd75b4b00f58873f66f0bd648
Yash Thakkar <yasht1407@gmail.com>
2023-10-23 10:38:19 +0530

    S46 - Campground Delete Middleware

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 // We are storing this in a separate variable just to shorten it up as we will need to make use of mongoose.Schema multiple times later on.
 const Schema = mongoose.Schema;
 
@@ -16,6 +17,20 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+})
+
+// The below middleware will run after we delete the campground
+CampgroundSchema.post('findOneAndDelete', async function(document) {
+    // The below document is the document that was just deleted.
+    // console.log(document);
+    if(document){
+        // We will be removing all the reviews present in the document reviews array.
+        await Review.deleteMany({
+            _id:{
+                $in: document.reviews
+            }
+        })
+    }
 })
 
 // Obviously, we can also store the below result in a variable and then export it
