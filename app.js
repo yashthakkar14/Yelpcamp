6db7b896ef4bdabbd1478a8aa5595b4db7ba469c
Yash Thakkar <yasht1407@gmail.com>
2023-10-24 12:25:42 +0530

    S49 - Breaking out Campground Routes

--- app.js
+++ app.js
@@ -11,30 +11,8 @@ const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
 
-const validateCampground = (req, res, next) => {
-    
-    const {error} = campgroundSchema.validate(req.body);
-    if(error){
-        // We need to do the below as details is basically an array of objects.
-        const msg = error.details.map(el=>el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    else{
-        next();
-    }
-}
-
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        // We need to do the below as details is basically an array of objects.
-        const msg = error.details.map(el=>el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    else{
-        next();
-    }
-}
+// Importing campground routes.
+const campgrounds = require('./routes/campgrounds');
 
 // Connecting to the database and then printing if the connection is successful or if we get an error.
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -55,51 +33,25 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
 
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body);
+    if(error){
+        // We need to do the below as details is basically an array of objects.
+        const msg = error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg, 400)
+    }
+    else{
+        next();
+    }
+}
+
+app.use('/campgrounds', campgrounds)
+
 app.get('/', (req, res)=>{
     res.render('home')
 })
 
-app.get('/campgrounds', catchAsync(async (req, res)=>{
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
-}));
 
-app.post('/campgrounds', validateCampground, catchAsync(async(req, res, next)=>{
-    // Basic rudimentary logic
-    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}))
-
-app.get('/campgrounds/new', (req, res)=>{
-    res.render('campgrounds/new');
-})
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res)=>{
-    const id = req.params.id;
-    const campground = await Campground.findById(id);
-    res.render('campgrounds/edit', {campground});
-}));
-
-
-app.get('/campgrounds/:id', catchAsync(async(req, res)=>{
-    const id = req.params.id;
-    const campground = await Campground.findById(id).populate('reviews');
-    res.render('campgrounds/show', {campground});
-}));
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async(req, res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground}, {new:true});
-    res.redirect(`/campgrounds/${campground._id}`)
-}));
-
-app.delete('/campgrounds/:id', catchAsync(async(req, res)=>{
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds')
-}));
 
 app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)=>{
     const campground = await Campground.findById(req.params.id);
