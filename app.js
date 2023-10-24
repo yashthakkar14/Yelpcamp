6f1dc4f6683707934a50ed0df42d845cefbad4c7
Yash Thakkar <yasht1407@gmail.com>
2023-10-24 12:44:19 +0530

    S49 - Breaking Review Routes

--- app.js
+++ app.js
@@ -2,17 +2,12 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
-// The below schemas are the Joi schema for the purpose of validation
-const {campgroundSchema, reviewSchema} = require('./schemas.js')
-const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
 
-// Importing campground routes.
+// Importing campground and review routes.
 const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews')
 
 // Connecting to the database and then printing if the connection is successful or if we get an error.
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -33,42 +28,13 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
 
-const validateReview = (req, res, next) => {
-    const {error} = reviewSchema.validate(req.body);
-    if(error){
-        // We need to do the below as details is basically an array of objects.
-        const msg = error.details.map(el=>el.message).join(',')
-        throw new ExpressError(msg, 400)
-    }
-    else{
-        next();
-    }
-}
-
-app.use('/campgrounds', campgrounds)
-
 app.get('/', (req, res)=>{
     res.render('home')
 })
 
-
-
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)=>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}));
-
-app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req, res)=>{
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, {"$pull":{reviews:reviewId}})
-    await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`);
-}))
-
+// Including all the campgrounds Routes.
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
 
 // This will only run if nothing else is matched first and we didn't respond from any of them.
 app.all('*', (req, res, next)=>{
