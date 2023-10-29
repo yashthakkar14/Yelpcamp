c0a03fd9a880d0a934fce9228796135d588efd48
Yash Thakkar <yasht1407@gmail.com>
2023-10-29 23:28:42 +0530

    S55 - Geocoding our Locations

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require('../models/campground');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const { cloudinary } = require('../cloudinary');
+const mapboxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapboxToken })
 
 module.exports.index = async (req, res)=>{
     const campgrounds = await Campground.find({});
@@ -11,14 +14,20 @@ module.exports.newCampgroundForm = (req, res)=>{
 }
 
 module.exports.createCampground = async(req, res, next)=>{
-    // we are doing an implicit return of dictionary so we need to wrap round brackets around the dictionary
-    const campground = new Campground(req.body.campground);
-    campground.images = req.files.map(f => ({url: f.path, filename: f.filename}))
-    campground.author = req.user._id;
-    await campground.save();
-    console.log(campground);
-    req.flash('success', 'Successfully made a new campground');
-    res.redirect(`/campgrounds/${campground._id}`)
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
+    console.log(geoData.body.features[0].geometry.coordinates);
+    res.send('OK!')
+    // // we are doing an implicit return of dictionary so we need to wrap round brackets around the dictionary
+    // const campground = new Campground(req.body.campground);
+    // campground.images = req.files.map(f => ({url: f.path, filename: f.filename}))
+    // campground.author = req.user._id;
+    // await campground.save();
+    // console.log(campground);
+    // req.flash('success', 'Successfully made a new campground');
+    // res.redirect(`/campgrounds/${campground._id}`)
 }
 
 module.exports.showCampground = async(req, res)=>{
