7690a2211985ae741c404edaec38186b37bed045
Yash Thakkar <yasht1407@gmail.com>
2023-10-30 17:19:54 +0530

    S56 - Adding Earthquake Cluster Map

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -37,7 +37,6 @@ module.exports.showCampground = async(req, res)=>{
         path:'author'
         }
     }).populate('author');
-    console.log(campground);
     if(!campground){
         req.flash('error', 'Cannot find that campground');
         return res.redirect('/campgrounds');
